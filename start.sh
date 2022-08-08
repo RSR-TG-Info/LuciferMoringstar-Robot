2beb191f9259c95b81b5fc7ed111e521239f56b5
Bikash Maity <110729541+PROFESSOR-9@users.noreply.github.com>
2022-08-08 09:02:31 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 echo "Cloning Repo, Please Wait..."
-git clone -b master https://github.com/PROFESSOR-9/LuciferMoringstar-Robot.git /IMDb-Movie-Bot
+git clone -b master https://github.com/PROFESSOR-9/LuciferMoringstar-Robot.git /LuciferMoringstar-Robot
 cd /LuciferMoringstar-Robot
 echo "Installing Requirements..."
 pip3 install -U -r requirements.txt
-echo "Starting Bot, Please Wait..."
+echo "Starting LuciferMoringstar Robot..."
 python3 bot.py
