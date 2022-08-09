8d029444089430ea3a608a3c262d719db4a3531f
Bikash Maity <110729541+PROFESSOR-9@users.noreply.github.com>
2022-08-09 19:54:43 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -1231,7 +1231,7 @@ async def auto_filter(client, msg, spoll=False):
             **locals()
         )
     else:
-        cap = f"<b>↪️ Requested: </b>{search}\n<b>📤 Uploaded To: </b>Movie Hub Server\n<b>🧑‍🔧 Get Support </b>✔️ <b><a href="https://t.me/hellodragan">TG Dragon</a></b>\n\n📌 Press The Down Buttons To Access The File\n<s>📌 This Post Will Be Deleted After 10 Minutes</s>"
+        cap = f"<b>🎬 Title:</b> {search}\n\n<b>👥 Requested by: {message.from_user.mention}</b>\n<b>© Powered By: <a href='https://t.me/+Rw47iXFdBF84YTNl'>{message.chat.title}</a></b>\n\n<b>✍️ Note:</b> <s>This message will be Auto-deleted after 10 minutes to avoid copyright issues.</s>"
     if imdb and imdb.get('poster'):
         try:
             hehe = await message.reply_photo(photo=imdb.get('poster'), caption=cap[:1024], reply_to_message_id=reply_id, reply_markup=InlineKeyboardMarkup(btn))
