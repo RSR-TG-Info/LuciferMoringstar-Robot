6813a0a956539b9dd9d5c03abc59960d01fb264f
Bikash Maity <110729541+PROFESSOR-9@users.noreply.github.com>
2022-08-10 09:18:37 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -8,7 +8,7 @@ logging.getLogger().setLevel(logging.ERROR)
 
 from pyrogram import Client, __version__
 from pyrogram.raw.all import layer
-from LuciferMoringstar-Robot.database.ia_filterdb import Media
+from database.ia_filterdb import Media
 from database.users_chats_db import db
 from info import SESSION, API_ID, API_HASH, BOT_TOKEN, LOG_STR
 from utils import temp
