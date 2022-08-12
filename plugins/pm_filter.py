5cae6b8a5ee763b366e291840bfbfccf69440e3a
Bikash Maity <110729541+PROFESSOR-9@users.noreply.github.com>
2022-08-12 20:03:50 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -1198,8 +1198,7 @@ async def auto_filter(client, msg, spoll=False):
                 [InlineKeyboardButton(text="🤖 Check Bot PM 🤖", url=f"https://t.me/{temp.U_NAME}")]
             )
     btn.insert(0, [
-        InlineKeyboardButton("ミ★ MOVIE CLUB ★彡", query.answer("Hi Boss", show_alert=True)
-    ])
+        InlineKeyboardButton("ミ★ MOVIE CLUB ★彡", query.answer("Hi Boss", show_alert=True))]
     reply_id = message.reply_to_message.message_id if message.reply_to_message else message.message_id
     imdb = await get_poster(search, file=(files[0]).file_name) if settings["imdb"] else None
     TEMPLATE = settings['template']
